edad81d1d9d6e41149a1dae7928ec8c21d7e5ae3
MobDev1130 <l00138041@student.lyit.ie>
2022-05-12 12:29:41 +0100

    Initial Commit
    Week 8 -
    cars prediction & murder rate data preparation

--- linear_regression.R
+++ linear_regression.R
@@ -77,8 +77,8 @@ predict(simple_linear_model, test_data, interval = "prediction")
 # y-axis = dependent var
 # If relationship exists then the linearity assumption is validated
 
-scatter.smooth(cars$speed, 
-               cars$dist, 
+scatter.smooth(cars$dist, 
+               cars$speed, 
                main = "Distance - speed", 
                xlab = "Car speed (mph)", 
                ylab = "Stopping distance")
@@ -108,3 +108,113 @@ par(opar)
 
 # Remove line 120 because it is an outlier
 cars <- subset(cars, cars$dist != 120)
+
+# Check that outlier row has been removed
+nrow(cars)
+
+par(mfrow = c(1,2))
+
+attach(cars)
+boxplot(speed, 
+        main = "Speed", 
+        sub = paste("Outlier rows: ", boxplot.stats(speed)$out))
+
+boxplot(dist, 
+        main = "Distance", 
+        sub = paste("Outlier rows: ", boxplot.stats(dist)$out))
+
+detach(cars)
+par(opar)
+
+# Check normality with histogram and Q-Q plot
+# Skewness function from e1071 library
+library(e1071)
+
+par(mfrow = c(1, 2))
+
+# density plot for speed
+plot(density(cars$speed), 
+     main = "Density plot : speed", 
+     ylab = "Frequency", 
+     xlab = "Speed", 
+     sub = paste("Skewness: ", round(e1071::skewness(cars$speed), 2)))
+
+# fill the area under the plot with a color
+# in this example it is Red
+polygon(density(cars$speed), col = "red")
+
+# skewness < -1 or > 1 is highly skewed
+# -1 to -0.5 and 0.5 to 1 - moderately skewed
+# -0.5 to 0.5 = approx symetrical
+
+plot(density(cars$dist), 
+     main = "Density plot : distance", 
+     ylab = "Frequency", 
+     xlab = "Distance", 
+     sub = paste("Skewness: ", round(e1071::skewness(cars$dist), 2)))
+polygon(density(cars$dist), col = "red")
+
+hist(cars$dist, 
+     main = "Normality proportion of distance", 
+     xlab = "Distance")
+
+qqnorm(cars$dist)
+qqline(cars$dist)
+
+par(opar)
+
+# split the data into training and testing
+# set the initial random variability seed
+set.seed(1)
+
+# Split the data into 70% training, 30% testing
+no_rows_data <- nrow(cars)
+sample_data <- sample(1:no_rows_data, 
+                      size = round(0.7 * no_rows_data), 
+                      replace = FALSE)
+
+training_data <- cars[sample_data, ]
+testing_data <- cars[-sample_data, ]
+
+linear_model <- lm(dist ~ speed, 
+                   data = training_data)
+summary(linear_model)
+
+AIC(linear_model)
+BIC(linear_model)
+
+# Predict values using the model
+# Distance will be predicted and compared to 
+# test data
+predicted_distance <- predict(linear_model, 
+                              testing_data)
+actuals_predictions <- data.frame(cbind(Actuals = testing_data$dist, 
+                                        Predicted = predicted_distance))
+actuals_predictions
+
+correlation_accuracy <- cor(actuals_predictions)
+correlation_accuracy
+
+# MAPE represents the mean forecast by which
+# the results will produce an error
+mape <- mean(abs(actuals_predictions$Predicted - 
+                   actuals_predictions$Actuals) / 
+               actuals_predictions$Actuals)
+mape
+
+library(lattice)
+install.packages("DAAG")
+library(DAAG)
+
+CV_results <- suppressWarnings(CVlm(data = cars, 
+                                    form.lm = dist ~ speed, 
+                                    dots = FALSE, 
+                                    seed = 1, 
+                                    legend.pos = "topleft", 
+                                    printit = FALSE,
+                                    main = "Small symbol are predicted values while bigger ones are actuals"))
+
+predicted_data <-data.frame(speed = c(10))
+
+predicted_distance <- predict(linear_model, predicted_data)
+predicted_distance
---
+++ murder_rate_prediction.R
@@ -0,0 +1,42 @@
+str(state.x77)
+
+# check the class of the data
+# show matrix and array
+class(state.x77)
+
+# convert the data to data frame
+# called state.x77_data
+states_data <- data.frame(state.x77)
+
+# check the structure of data
+str(states_data)
+
+# Move state name into new variable
+states_data$name <- state.name
+
+# rename the Life exp and HS grad variables 
+# to Life_exp and HS_grade
+colnames(states_data)[colnames(states_data) == "Life.Exp"] <- "Life_exp"
+colnames(states_data)[colnames(states_data) == "HS.Grad"] <- "HS_grade"
+colnames(states_data)
+
+numeric_variable <- sapply(states_data, is.numeric)
+numeric_data <- states_data[numeric_variable]
+
+pairs(numeric_data)
+
+library(psych)
+pairs.panels(states_data,
+             smooth = TRUE,
+             scale = FALSE,
+             density = TRUE,
+             ellipses = TRUE,
+             method = "spearman",
+             pch = 21,
+             lm = FALSE, 
+             cor = TRUE,
+             jiggle = FALSE,
+             factor = 2,
+             hist.col = 4,
+             stars = TRUE,
+             ci = TRUE) 
