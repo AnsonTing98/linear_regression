cce68a2692cf4968d3b31887056196a84e0fea08
MobDev1130 <l00138041@student.lyit.ie>
2022-05-19 12:22:44 +0100

    Initial Commit
    Week 9 - insurance linear regression model

--- insurance.R
+++ insurance.R
@@ -14,7 +14,6 @@ summary(factor(insurance_data$region))
 insurance_data$sex <- factor(insurance_data$sex, 
                              levels = c("male", "female"), 
                              ordered = FALSE)
-str(insurance_data)
 
 insurance_data$smoker <- factor(insurance_data$smoker, 
                                 levels = c("yes", "no"), 
@@ -26,6 +25,8 @@ insurance_data$region <- factor(insurance_data$region,
                                            "southeast", 
                                            "southwest"), 
                                 ordered = FALSE)
+
+str(insurance_data)
 
 library(psych)
 
@@ -49,11 +50,183 @@ pairs.panels(insurance_data,
 # will automatically split the factor variables
 # Alternatively want to control the model build process
 
+# in linear regresison, mode lis:
+# y = b0 + B1x1 + B2x2 + B3x3 + e
+# where y = insurance charges
+# x1 = age of the person
+# x2  sex of the person
+# x3 = bmi of the person
+# x4 = children
+# x5 = smoker
+# x6 = region
+# It is clear that x1, and x3 are continuous and x2, x4, x5, x6 are categorical
+# therefore we need to create dummy variables for the categorical
+# variables
+# For the sex variable x2
+# x2 = 1 if person is male
+# x2 = 0 if person is female
+
 set.seed(1)
 model <- lm(formula = charges ~ age + sex + bmi + children + smoker + region, 
             data = insurance_data)
 
 summary(model)
+
+# obviously age, bmi, children, smokerno have an 
+# influence over the dependent variable "charges"
+# p-value less than the sig value
+# sexfemale is not influential on the model
+# keep region for now as need to use it
+# for the research question
+insurance_data <- insurance_data[c(1, 3:7)]
+
+# Round the BMI and charges values
+insurance_data$bmi <- round(insurance_data$bmi, 1)
+insurance_data$charges <- round(insurance_data$charges, 2)
+
+# Create the model again with the amended changes
+model <- lm(formula = charges ~ age + bmi + children + smoker + region, 
+            data = insurance_data)
+
+summary(model)
+
+# now check the model assumption
+# Linearity
+# examine whether a linear
+# correlation exists between continuous variable
+scatter.smooth(insurance_data$age, 
+               insurance_data$charges, 
+               main = "Insurance charges ~ age", 
+               ylab = "Insurance charges (,000)", 
+               xlab = "Age (years)")
+
+# BMI
+scatter.smooth(insurance_data$bmi, 
+               insurance_data$charges, 
+               main = "Insurance charges ~ bmi", 
+               ylab = "Insurance charges (,000)", 
+               xlab = "BMI")
+
+scatter.smooth(insurance_data$children, 
+               insurance_data$charges, 
+               main = "Insurance charges ~ children", 
+               ylab = "Insurance charges (,000)", 
+               xlab = "Children")
+
+scatter.smooth(insurance_data$smoker, 
+               insurance_data$charges, 
+               main = "Insurance charges ~ smoker", 
+               ylab = "Insurance charges (,000)", 
+               xlab = "Smoker")
+
+scatter.smooth(insurance_data$region, 
+               insurance_data$charges, 
+               main = "Insurance charges ~ region", 
+               ylab = "Insurance charges (,000)", 
+               xlab = "Region")
+
+# No info available for plot of continuous versus categorical
+# Instead examine relationship with dependent
+# and categorical data using a bar plot
+scatter.smooth(insurance_data$smoker, 
+               insurance_data$charges, 
+               main = "Insurance charges ~ smoker", 
+               ylab = "Insurance charges (,000)", 
+               xlab = "Smoker")
+
+plot(insurance_data$smoker, 
+     insurance_data$charges, 
+     main = "Charges by smoker status", 
+     xlab = "Smoker", 
+     ylab = "Insurance charges")
+
+plot(insurance_data$region, 
+     insurance_data$charges, 
+     main = "Charges by region", 
+     xlab = "Region", 
+     ylab = "Insurance charges")
+
+# Only use this method if
+# sure about the underlying nature
+# of the variables that are correlating
+insurance_data$cor_smoker <- ifelse(insurance_data$smoker == "yes", 1, 0)
+
+cor(insurance_data$charges, insurance_data$cor_smoker)
+
+# Normality
+with(insurance_data, {qqnorm(age, 
+         main = "Normality analytics of age data") 
+  qqline(age)
+  })
+
+# Repeat for other continuous data
+with(insurance_data, {qqnorm(charges[smoker == "yes"], 
+                             main = "Normality analytics of smoker = 'yes' data") 
+  qqline(charges[smoker == "yes"])
+})
+
+with(insurance_data, {qqnorm(charges[smoker == "no"], 
+                             main = "Normality analytics of smoker = 'no' data") 
+  qqline(charges[smoker == "no"])
+})
+
+with(insurance_data, {qqnorm(charges[region == "southwest"], 
+                             main = "Normality analytics of regional southwest data") 
+  qqline(charges[region == "southwest"])
+})
+
+with(insurance_data, {qqnorm(charges[region == "southeast"],
+                             main = "Normality analytics of regional southeast data") 
+  qqline(charges[region == "southeast"])
+})
+
+with(insurance_data, {qqnorm(charges[region == "northwest"],
+                             main = "Normality analytics of regional northwest data") 
+  qqline(charges[region == "northwest"])
+})
+
+with(insurance_data, {qqnorm(charges[region == "northeast"],
+                             main = "Normality analytics of regional northeast data") 
+  qqline(charges[region == "northeast"])
+})
+
+qqnorm(insurance_data$age)
+qqline(insurance_data$age, col = "red")
+
+# 
+shapiro.test(insurance_data$age)$p.value
+shapiro.test(insurance_data$bmi)$p.value
+shapiro.test(insurance_data$children)$p.value
+shapiro.test(insurance_data$charges)$p.value
+
+region_normality_test <- with(insurance_data, tapply(charges, region, shapiro.test))
+region_normality_test$northeast$p.value
+region_normality_test$northwest$p.value
+region_normality_test$southeast$p.value
+region_normality_test$southwest$p.value
+
+smoker_normality_test <- with(insurance_data, tapply(charges, smoker, shapiro.test))
+smoker_normality_test$yes$p.value
+smoker_normality_test$no$p.value
+
+# Measure colinearity which is the relationship
+# between multiple variables
+# The "tolerance" is an indication of the percent
+# variance in the predictor that cannot
+# be a counted for by the other predictors
+# hence very small values indicate a predictor is redundant
+
+# VIF score should be close to 1 but under 5
+# 10+ indicates that the variable is not needed
+# and can be removed from the model
+library(car)
+vif(model)
+
+with(insurance_data, qqplot(charges, male, 
+                            main = "Comparing two samples of activity data", 
+                            xlab = "Insurance charges (,000)", 
+                            ylab = "Male"))
+abline(0, 1)
 
 # Instead control the process
 # Create a own variables
@@ -72,10 +245,6 @@ insurance_data$sw <- ifelse(insurance_data$region == "southwest", 1, 0)
 # using age, male, female, BMI, smoker, nonsmoker
 # ne, nw, se, sw
 insurance_data <- insurance_data[c(1, 3, 4, 7:15)]
-
-# Round the BMI and charges values
-insurance_data$bmi <- round(insurance_data$bmi, 1)
-insurance_data$charges <- round(insurance_data$charges, 2)
 
 # check model assumptions
 # Linearity
@@ -156,3 +325,5 @@ qqPlot(model,
        id.method = "identify", 
        simulate = TRUE, 
        main = "Q-Q plot")
+
+
